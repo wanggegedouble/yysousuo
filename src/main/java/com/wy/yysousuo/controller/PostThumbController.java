5f77c04eb679d6dc148b3e2e2700f1861c59788a
wy <2511933030@qq.com>
2023-12-29 14:43:10 +0800

    init

---
+++ src/main/java/com/wy/yysousuo/controller/PostThumbController.java
@@ -0,0 +1,54 @@
+package com.wy.yysousuo.controller;
+
+import com.wy.yysousuo.common.BaseResponse;
+import com.wy.yysousuo.common.ErrorCode;
+import com.wy.yysousuo.common.ResultUtils;
+import com.wy.yysousuo.exception.BusinessException;
+import com.wy.yysousuo.model.dto.postthumb.PostThumbAddRequest;
+import com.wy.yysousuo.model.entity.User;
+import com.wy.yysousuo.service.PostThumbService;
+import com.wy.yysousuo.service.UserService;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 帖子点赞接口
+ 
+ */
+@RestController
+@RequestMapping("/post_thumb")
+@Slf4j
+public class PostThumbController {
+
+    @Resource
+    private PostThumbService postThumbService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 点赞 / 取消点赞
+     *
+     * @param postThumbAddRequest
+     * @param request
+     * @return resultNum 本次点赞变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doThumb(@RequestBody PostThumbAddRequest postThumbAddRequest,
+            HttpServletRequest request) {
+        if (postThumbAddRequest == null || postThumbAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postThumbAddRequest.getPostId();
+        int result = postThumbService.doPostThumb(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+}
